5d0d602e120119579c7e889ca4dcb471ea5f5caf
Yas Abdolmaleki <yasabdolmaleki@gmail.com>
2019-04-22 13:53:59 -0400

    Initial Commit

---
+++ AnalogTimePicker/AnalogTimePicker.h
@@ -0,0 +1,19 @@
+//
+//  AnalogTimePicker.h
+//  AnalogTimePicker
+//
+//  Created by Yas Abdolmaleki on 2019-04-22.
+//  Copyright © 2019 Yas Marcu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnalogTimePicker.
+FOUNDATION_EXPORT double AnalogTimePickerVersionNumber;
+
+//! Project version string for AnalogTimePicker.
+FOUNDATION_EXPORT const unsigned char AnalogTimePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalogTimePicker/PublicHeader.h>
+
+
